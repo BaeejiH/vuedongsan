7f25fe78eb3bcb3a0c37cd49acf9475073390c3c
BaeejiH <bjihun0511@gmail.com>
2024-10-17 16:00:19 +0900

    데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  vue.js로 만드는 부동산 프로젝트
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2 :style="스타일"> {{products[0]}}원룸</h2>
+    <p>500만원</p>
+  </div>
+
+  <div>
+    <h2 :style="스타이일">{{products[1]}} 원룸</h2>
+    <p>700만원</p>
+  </div>
+
+  <div>
+    <h2 :style="스타이일">{{products[2]}} 원룸</h2>
+    <p>1000만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      //데이터 바인딩-> 실시간 자동 렌더링을 쓰기위해서
+      //여기에 모든 데이터 저장해둬야 함
+      //데이터는 odject 자료로 저장
+      //데이터를 위에다 넣어줄 수 있음
+      pricel1 : 5000,
+      pricel2 : 7000,
+      rogo : '원룸',
+      스타일 : 'color : red',
+      스타이일 : 'color : blue',
+      products : ['역상동 원룸','천호동원룸','마포구원룸']
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
